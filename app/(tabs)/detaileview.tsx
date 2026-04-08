e8551450f6f04ddfee94816fa6fc4a71e2541125
karl-ha <karl.soderholm@ha.ax>
2026-04-08 20:58:13 +0300

    more complete detailedview.tsx

--- app/(tabs)/detaileview.tsx
+++ app/(tabs)/detaileview.tsx
@@ -1,7 +1,7 @@
+import { useLocalSearchParams } from "expo-router";
 import React, { useEffect, useState } from "react";
 import { Text, View } from "react-native";
-import { Avatar} from "react-native-paper";
-import { useLocalSearchParams } from 'expo-router';
+import { Avatar } from "react-native-paper";
 
 export default function DetailView() {
   const { id, first_name, last_name, image } = useLocalSearchParams();
@@ -9,58 +9,27 @@ export default function DetailView() {
 
   useEffect(() => {
     fetch(`https://lagtinget.ax{id}.json`)
-      .then(res => res.json())
-      .then(data => setExtra(data));
+      .then((res) => res.json())
+      .then((data) => setExtra(data));
   }, [id]);
-}
 
   return (
-    <View style={styles.container}>
-      <View style={styles.searchContainer}>
-        <Searchbar
-          placeholder="Sök ledamot..."
-          onChangeText={updateSearch}
-          value={search}
-        />
-      </View>
-      <View style={{ flex: 1 }}>
-        {loading ? (
-          <ActivityIndicator size="large" style={{ marginTop: 50 }} />
-        ) : (
-          <FlatList
-            data={filteredLedamoter}
-            keyExtractor={(item) => item.id.toString()}
-            renderItem={({ item }) => (
-              <List.Item
-                title={`${item.first_name} ${item.last_name}`}
-                left={(props) =>
-                  item.image && item.image.url ? (
-                    <Avatar.Image
-                      {...props}
-                      size={48}
-                      source={{ uri: item.image.url }}
-                    />
-                  ) : (
-                    <Avatar.Icon {...props} size={48} icon="account" />
-                  )
-                }
-              />
-            )}
-          />
-        )}
-      </View>
+    <View style={{ flex: 1, padding: 20, alignItems: "center" }}>
+      {image ? (
+        <Avatar.Image size={100} source={{ uri: image as string }} />
+      ) : null}
+      <Text style={{ fontSize: 20, fontWeight: "bold" }}>
+        {first_name} {last_name}
+      </Text>
+      {extra && (
+        <View style={{ marginTop: 20, width: "100%" }}>
+          <Text>Phone: {extra.phone}</Text>
+          <Text>Email: {extra.email}</Text>
+          <Text>Profession: {extra.profession}</Text>
+          <Text>Birthday: {extra.birthday}</Text>
+          <Text>Adress: {extra.adress}</Text>
+        </View>
+      )}
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-  },
-  searchContainer: {
-    paddingTop: 50,
-    paddingHorizontal: 10,
-    paddingBottom: 10,
-  },
-});
