399ba057b295db25d06abe0d318513e81ed10916
karl-ha <karl.soderholm@ha.ax>
2026-04-08 17:51:44 +0300

    more work on API

--- app/(tabs)/explore.tsx
+++ app/(tabs)/explore.tsx
@@ -8,20 +8,20 @@ export default function ExploreScreen() {
   const [ledamot, setLedamoter] = useState<Ledamot[]>([]);
   const [loading, setLoading] = useState(true);
 
-const DATA = [
-  {
-    id: 'bd7acbea-c1b1-46c2-aed5-3ad53abb28ba',
-    title: 'First Item',
-  },
-  {
-    id: '3ac68afc-c605-48d3-a4f8-fbd91aa97f63',
-    title: 'Second Item',
-  },
-  {
-    id: '58694a0f-3da1-471f-bd96-145571e29d72',
-    title: 'Third Item',
-  },
-];
+  useEffect(() => {
+    const fetchLedamoter = async () => {
+      try {
+        const response = await fetch('https://lagtinget.ax');
+        const data = await response.json();
+        setLedamoter(data);
+      } catch (error) {
+        console.error('Error:', error);
+      } finally {
+        setLoading(false);
+      }
+    };
+    fetchLedamoter();
+  }, []);
 
 type ItemProps = {title: string};
 
