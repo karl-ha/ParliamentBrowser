e15f7ab29069d2fd4216ba3ff6b1112be6631e8f
karl-ha <karl.soderholm@ha.ax>
2026-04-08 18:19:35 +0300

    working list of parlaiment members, however also shows those who are not currently members

--- app/(tabs)/explore.tsx
+++ app/(tabs)/explore.tsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from 'react';
 import { View, FlatList, ActivityIndicator } from 'react-native';
-import { List, Text } from 'react-native-paper';
+import { List} from 'react-native-paper';
 
 type Ledamot = { id: number; first_name: string; last_name: string };
 
@@ -11,7 +11,7 @@ export default function ExploreScreen() {
   useEffect(() => {
     const fetchLedamoter = async () => {
       try {
-        const response = await fetch('https://lagtinget.ax');
+        const response = await fetch('https://api.lagtinget.ax/api/persons.json');
         const data = await response.json();
         setLedamoter(data);
       } catch (error) {
@@ -23,40 +23,22 @@ export default function ExploreScreen() {
     fetchLedamoter();
   }, []);
 
-type ItemProps = {title: string};
-
-const Item = ({title}: ItemProps) => (
-  <View style={styles.item}>
-    <Text style={styles.title}>{title}</Text>
-  </View>
-);
-
-const App = () => (
-  <SafeAreaProvider>
-    <SafeAreaView style={styles.container}>
-      <FlatList
-        data={DATA}
-        renderItem={({item}) => <Item title={item.title} />}
-        keyExtractor={item => item.id}
-      />
-    </SafeAreaView>
-  </SafeAreaProvider>
-);
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    marginTop: StatusBar.currentHeight || 0,
-  },
-  item: {
-    backgroundColor: '#f9c2ff',
-    padding: 20,
-    marginVertical: 8,
-    marginHorizontal: 16,
-  },
-  title: {
-    fontSize: 32,
-  },
-});
-
-export default App;
+  return (
+    <View style={{ flex: 1, paddingTop: 50 }}>
+      {loading ? (
+        <ActivityIndicator size="large" />
+      ) : (
+        <FlatList
+          data={ledamot}
+          keyExtractor={item => item.id.toString()}
+          renderItem={({ item }) => (
+            <List.Item
+              title={`${item.first_name} ${item.last_name}`}
+              left={props => <List.Icon {...props} icon="account" />}
+            />
+          )}
+        />
+      )}
+    </View>
+  );
+}
